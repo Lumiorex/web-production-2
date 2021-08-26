b580c3625e3c2903772360c10315ebeae9903498
Lumiorex <kelvin.shi35@myhunter.cuny.edu>
2021-08-26 12:41:58 -0400

    implemented the javascript file and began on javascript code

---
+++ script.js
@@ -0,0 +1,9 @@
+let me = (name: "Kelvin", adjectives: ["extremely rusty at coding, couch potato, likes to read"]);
+
+
+
+
+function describeMe()
+{
+  document.querySelector("desciption").innerHTML = me, name;
+}
